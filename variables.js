d82b6b2ea10efef39637a8f557a58c6671db10c3
Craig McGinley <craigmcginley@gmail.com>
2021-12-06 21:44:33 -0500

    use regular discord id variable, add deploy for post install for heroku

--- variables.js
+++ variables.js
@@ -5,7 +5,7 @@ dotenv.config();
 module.exports = {
   token: process.env.DISCORD_TOKEN,
   clientId: process.env.DISCORD_CLIENT_ID,
-  guildId: process.env.DISCORD_GUILD_ID_ME,
+  guildId: process.env.DISCORD_GUILD_ID,
   verificationChannelId: process.env.VERIFICATION_CHANNEL_ID,
   welcomeChannelId: process.env.WELCOME_CHANNEL_ID,
   verifiedRole: process.env.VERIFIED_ROLE,
