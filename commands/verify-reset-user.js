aaa5fa20bbf9b9e8135b2074bb79196ef140e3e3
Craig McGinley <craigmcginley@gmail.com>
2021-12-17 00:48:08 -0500

    add command to remove user from db

---
+++ commands/verify-reset-user.js
@@ -0,0 +1,38 @@
+const { Permissions } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const { models } = require('../db/sequelize.js');
+
+const { Guild, Member } = models;
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('verify-reset-user')
+    .setDescription('Reset the user\'s application so they can trigger the bot again.')
+    .addUserOption(option =>
+      option.setName('user')
+        .setDescription('The user to reset.')
+        .setRequired(true)),
+    async execute(interaction) {
+      if (!interaction.member.permissions.has(Permissions.FLAGS.MANAGE_ROLES)) {
+        await interaction.reply("You don't have permission to use this.");
+        return;
+      }
+
+      const discordUser = await interaction.options.getUser('user');
+
+      const member = await Member.findOne({
+        where: {
+          discord_id: discordUser.id
+        }
+      });
+
+      if (member) {
+        await member.destroy();
+
+        await interaction.reply(`Finished! Removed ${discordUser.username} from database`);
+      } else {
+        await interaction.reply(`Couldn't find that user in the database!`);
+      }
+  },
+};
